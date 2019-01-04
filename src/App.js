d72726e4a5919c39268974af894a458ab84c98b1
Valeriy Shinkevich <v.v.shinkevich@gmail.com>
2019-01-04 13:10:24 +0300

    App class component with two functional components (Search. Table)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+  {
+    title: "Lombard-Service",
+    url: "https://lombard-service.ru/",
+    author: "Valeriy Shinkevich",
+    num_comments: 1,
+    points: 10,
+    objectID: 2
+  }
+];
+
+const Search = ({value,onChange}) => 
+  <form>
+    <input type="text" value={value} onChange={onChange} />
+  </form>;
+
+const termFilter = term => ({title}) => term === '' || title.toLowerCase().includes(term.toLowerCase());
+
+const Table = ({list, pattern, onDismiss}) => list
+  .filter(termFilter(pattern))
+  .map(item => {
+    const onHandleDismiss = () => onDismiss(item.objectID);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>
+            {item.title}
           </a>
-        </header>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <button onClick={onHandleDismiss} type="button">X</button>
+        </span>
       </div>
     );
+  })
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(event){
+    this.setState({searchTerm: event.target.value});
+  }
+
+  render() {
+
+    const {list, searchTerm} = this.state;
+
+    return (
+      <div className="App">
+        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>       
+      </div>
+    );
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 }
 
