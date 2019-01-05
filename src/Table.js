c49b801663cf6bbc31b8a1eeef9347fb9539543d
Valeriy Shinkevich <v.v.shinkevich@gmail.com>
2019-01-05 16:17:46 +0300

    Format  code

--- src/Table.js
+++ src/Table.js
@@ -4,24 +4,24 @@ import Button from './Button'
 const termFilter = term => ({title}) => term === '' || title.toLowerCase().includes(term.toLowerCase());
 
 const Table = ({list, pattern, onDismiss}) => list
-.filter(termFilter(pattern))
-.map(item => {
-    const onHandleDismiss = () => onDismiss(item.objectID);
-    return (
-    <div key={item.objectID}>
-        <span>
-            <a href={item.url}>
-                {item.title}
-            </a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-        <Button onClick={onHandleDismiss} className="dismiss">X</Button>
-        </span>
-    </div>
-    );
-})
+    .filter(termFilter(pattern))
+    .map(item => {
+        const onHandleDismiss = () => onDismiss(item.objectID);
+        return (
+        <div key={item.objectID}>
+            <span>
+                <a href={item.url}>
+                    {item.title}
+                </a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+            <Button onClick={onHandleDismiss} className="dismiss">X</Button>
+            </span>
+        </div>
+        );
+    })
 
 export default Table
